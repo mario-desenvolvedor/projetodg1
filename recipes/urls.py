b28618043e91c9d1154cb27db175dd579b7a0ed1
mario sergio santos <mariosergiosantos65@gmail.com>
2025-06-25 20:38:59 -0300

    finalizando gride

---
+++ recipes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from recipes.views import home, contato
+
+
+urlpatterns = [
+    
+    path("", home), # Home
+    
+    path("contato", contato), # /contato/
+    
+    
+    
+]
---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render (request, 'global/home.html')
+ 
+
+def contato(request):
+    return HttpResponse('contato')
+
